3ad65191e1a66bfcc8f990f6e529ed791b8cd59c
Muhammad Abdullah <abdullahtariq52@gmail.com>
2018-10-15 14:16:22 +0500

    add map transform

---
+++ src/Transform/Map.ts
@@ -0,0 +1,31 @@
+interface Storage {
+  name: string
+  body: object
+  proto: string
+}
+
+/**
+ * Serialize a Map into JSON
+ */
+export default {
+  type: 'Map',
+  shouldTransform(type: any, obj: any) {
+    return obj.constructor.name === 'Map'
+  },
+  toSerializable(map: any): Storage {
+    let body = {}
+
+    map.forEach(function(value, key) {
+      body[key] = value
+    })
+
+    return {
+      name: 'Map',
+      body,
+      proto: Object.getPrototypeOf(map).constructor.name
+    }
+  },
+  fromSerializable(data: Storage) {
+    return data
+  }
+}
