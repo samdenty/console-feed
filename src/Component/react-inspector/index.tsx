a673b5f13a0f5647f9501ca8578307a34037e62d
just.doit <just.doit@kakaopaycorp.com>
2020-08-06 19:00:37 +0900

    fix: fix import 'ObjectPreview'

--- src/Component/react-inspector/index.tsx
+++ src/Component/react-inspector/index.tsx
@@ -6,9 +6,9 @@ import {
   ObjectLabel,
   ObjectName,
   ObjectRootLabel,
-  ObjectValue
+  ObjectValue,
+  ObjectPreview
 } from 'react-inspector'
-import { ObjectPreview } from 'react-inspector/dist/cjs/react-inspector'
 
 import { Context } from '../../definitions/Component'
 import { Constructor, HTML, Root, Table } from './elements'
