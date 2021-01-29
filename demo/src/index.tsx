1cf5911cfa06d7366a5eeb6e974c01fd4f2c47b4
Samuel Bodin <samuel.bodin@algolia.com>
2021-01-29 10:47:29 +0100

    update doc + fix demo

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import update from 'immutability-helper'
 import { Hook, Console, Decode } from '../../src'
